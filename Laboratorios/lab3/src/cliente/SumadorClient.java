d4ddc0de49b26ef46fa8d7a96f0ab609bd8d728e
Roberth Santiago Mendez Rivera <mendezr.roberth@javeriana.edu.co>
2025-08-26 23:46:35 -0500

    Se comentó todo el codigo de manera descriptiva haciendo enfasis en los metodos que se implementan de la clase rmi

--- Laboratorios/lab3/src/cliente/SumadorClient.java
+++ Laboratorios/lab3/src/cliente/SumadorClient.java
@@ -1,32 +1,37 @@
-// Permite ejecutar métodos de objetos ubicados en otra máquina con Remote Method Invocation
-import java.rmi.*;
+import java.rmi.*; // Permite ejecutar métodos de objetos ubicados en otra máquina con Remote Method Invocation
 
 public class SumadorClient {
 
-  // Punto de entrada del programa.
   //Los argumentos que se pasen por consola estarán en el arreglo 'args'. Se espera que el primer argumento sea la dirección del servidor RMI.
   public static void main(String args[]) {
 
-    int res = 0; // Variable local donde se guardará el resultado de la operación remota.
+    // Variable local donde se guardará el resultado de la operación remota.
+    int res = 0; 
 
     try {
 
-      System.out.println("Buscando Objeto "); // Mensaje para saber que el cliente está buscando el objeto remoto.
+      // Mensaje para saber que el cliente está buscando el objeto remoto.
+      System.out.println("Buscando Objeto "); 
 
-      // Se hace un casting a la interfaz "Sumador" para poder usar sus métodos.
-      // Se utiliza la clase Naming para localizar el objeto remoto.
-      // "rmi://" indica que se usará RMI.
+      /*  
+       * Se hace un casting a la interfaz "Sumador" para poder usar sus métodos.
+       * Se utiliza la clase Naming para localizar el objeto remoto.
+       * "rmi://" indica que se usará RMI.
+       */
       Sumador misuma = (Sumador)Naming.lookup("rmi://" + args[0] + "/" +"MiSumador");
 
-      res = misuma.sumar(5, 2); // Aquí se suman los números 5 y 2 en el servidor, y el resultado se devuelve al cliente.
+      // Aquí se suman los números 5 y 2 en el servidor, y el resultado se devuelve al cliente.
+      res = misuma.sumar(5, 2); 
 
-      System.out.println("5 + 2 = " + res); //Aqui se imprime el resultado de la operacion obtenido desde el servidor
+      //Aqui se imprime el resultado de la operacion obtenido desde el servidor
+      System.out.println("5 + 2 = " + res); 
 
     } catch(Exception e) {
-      // Si ocurre cualquier excepción se captura aquí.
+        // Si ocurre cualquier excepción se captura aquí.
         System.err.println(" System exception");
     }
-    //finaliza la ejecucion del programa
+
+    //Finaliza la ejecucion del programa
     System.exit(0);
 
   }
