33ea734992dce358110e092833328abdef8e3fda
Mariana Osorio Vásquez <145573538+mariana-osoriov827@users.noreply.github.com>
2025-10-06 22:11:31 -0500

    Update Ejemplar.java

--- Proyecto/src/Gestor_Almacenamiento/Ejemplar.java
+++ Proyecto/src/Gestor_Almacenamiento/Ejemplar.java
@@ -1,3 +1,15 @@
+/**************************************************************************************
+* Fecha: 10/10/2025
+* Autor: Gabriel Jaramillo, Roberth Méndez, Mariana Osorio Vasquez, Juan Esteban Vera
+* Tema: 
+* - Proyecto préstamo de libros (Sistema Distribuido)
+* Descripción:
+* - Clase Entidad (Ejemplar):
+* - Representa una copia física única de un libro.
+* - Almacena el estado ('D' = Disponible, 'P' = Prestado) y la fecha de devolución si está prestado.
+* - Implementa Serializable para permitir la transferencia de objetos a través de 
+* RMI y para la persistencia en el Gestor de Almacenamiento (GA).
+***************************************************************************************/
 import java.io.Serializable;
 
 public class Ejemplar implements Serializable {
@@ -19,3 +31,4 @@ public class Ejemplar implements Serializable {
     public void setEstado(char estado) { this.estado = estado; }
     public void setFecha(String fecha) { this.fecha = fecha; }
 }
+
