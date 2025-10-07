e241d5cd76d253b719738e374c010770852a4339
Mariana Osorio Vásquez <145573538+mariana-osoriov827@users.noreply.github.com>
2025-10-06 22:20:49 -0500

    Update Message.java

--- Proyecto/src/Gestor_carga/Message.java
+++ Proyecto/src/Gestor_carga/Message.java
@@ -1,3 +1,17 @@
+/**************************************************************************************
+* Fecha: 10/10/2025
+* Autor: Gabriel Jaramillo, Roberth Méndez, Mariana Osorio Vasquez, Juan Esteban Vera
+* Tema: 
+* - Proyecto préstamo de libros (Sistema Distribuido)
+* Descripción:
+* - Clase Entidad de Mensajería (Message):
+* - Define la estructura de datos utilizada para encapsular una solicitud de operación dentro del sistema distribuido.
+* - Funciona como un evento o un comando en el patrón de mensajería asíncrona.
+* - Contiene campos esenciales como el ID (para seguimiento del estado), el Topic (Devolucion o Renovacion), y los datos de la operación (código de libro, ID de usuario).
+* - Implementa Serializable para ser transferida por RMI entre el Gestor de Carga 
+* y los Actores.
+***************************************************************************************/
+
 import java.io.Serializable;
 
 public class Message implements Serializable {
