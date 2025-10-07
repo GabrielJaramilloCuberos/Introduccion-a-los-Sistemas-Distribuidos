b0d6234ec12e46f72a0d1ca95e0c9e9d650fff76
Mariana Osorio Vásquez <145573538+mariana-osoriov827@users.noreply.github.com>
2025-10-06 22:12:27 -0500

    Update GestorAlmacenamiento.java

--- Proyecto/src/Gestor_Almacenamiento/GestorAlmacenamiento.java
+++ Proyecto/src/Gestor_Almacenamiento/GestorAlmacenamiento.java
@@ -1,3 +1,15 @@
+/**************************************************************************************
+* Fecha: 10/10/2025
+* Autor: Gabriel Jaramillo, Roberth Méndez, Mariana Osorio Vasquez, Juan Esteban Vera
+* Tema: 
+* - Proyecto préstamo de libros (Sistema Distribuido)
+* Descripción:
+* - Interfaz de Servicio Remoto (GestorAlmacenamiento):
+* - Define el contrato RMI (`Remote`) para que los Actores (Renovación/Devolución) 
+* puedan invocar remotamente las operaciones de modificación de datos sobre la Base de Datos local.
+* - Incluye métodos para la aplicación de Devolución y Renovación, así como 
+* para la replicación de cambios con el GA de la otra sede.
+***************************************************************************************/
 import java.rmi.Remote;
 import java.rmi.RemoteException;
 
