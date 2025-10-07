6e445aa4a03868507397b2e03e6dbced51f829b1
Mariana Osorio Vásquez <145573538+mariana-osoriov827@users.noreply.github.com>
2025-10-06 22:19:10 -0500

    Update BibliotecaGC.java

--- Proyecto/src/Gestor_carga/BibliotecaGC.java
+++ Proyecto/src/Gestor_carga/BibliotecaGC.java
@@ -1,3 +1,19 @@
+/**************************************************************************************
+* Fecha: 10/10/2025
+* Autor: Gabriel Jaramillo, Roberth Méndez, Mariana Osorio Vasquez, Juan Esteban Vera
+* Tema: 
+* - Proyecto préstamo de libros (Sistema Distribuido)
+* Descripción:
+* - Clase Interfaz de Servicio Remoto (BibliotecaGC):
+* - Define el contrato RMI (`Remote`) para el Gestor de Carga (GC).
+* - Proporciona los métodos asíncronos (`*Async`) que usan los clientes 
+* para enviar solicitudes de operación.
+* - Proporciona los métodos de mensajería (`fetchNextMessage`, `ackMessage`) 
+* que usan los Actores para consumir y confirmar la correcta recepción de eventos, 
+* simulando el patrón PUB/SUB.
+* - Permite a los clientes consultar el estado final de una operación (`getMessageStatus`)
+* usando el ID del mensaje.
+***************************************************************************************/
 import java.rmi.Remote;
 import java.rmi.RemoteException;
 
