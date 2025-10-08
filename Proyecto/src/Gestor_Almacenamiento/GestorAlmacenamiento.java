eb8a60a6a3bc429de6c8773fbd194a4961912b4f
Juan Esteban Vera Garzon <167147949+Epistoler32@users.noreply.github.com>
2025-10-08 09:30:56 -0500

    Update GestorAlmacenamiento.java
    add Prestamo operations to the class

--- Proyecto/src/Gestor_Almacenamiento/GestorAlmacenamiento.java
+++ Proyecto/src/Gestor_Almacenamiento/GestorAlmacenamiento.java
@@ -2,8 +2,11 @@ import java.rmi.Remote;
 import java.rmi.RemoteException;
 
 public interface GestorAlmacenamiento extends Remote {
-    public boolean aplicarDevolucionEnBD(String codigoLibro, String usuarioId) throws RemoteException;
-    public boolean aplicarRenovacionEnBD(String codigoLibro, String usuarioId, String nuevaFechaEntrega) throws RemoteException;
-    public String dumpDB() throws RemoteException;
-    public void replicarOperacion(String op, String codigoLibro, String usuarioId, String fecha) throws RemoteException;
+
+    boolean aplicarDevolucionEnBD(String codigoLibro, String usuarioId) throws RemoteException;
+    boolean aplicarRenovacionEnBD(String codigoLibro, String usuarioId, String nuevaFechaEntrega) throws RemoteException;
+    boolean aplicarPrestamoEnBD(String codigoLibro, String usuarioId) throws RemoteException;
+
+    String dumpDB() throws RemoteException;
+    void replicarOperacion(String op, String codigoLibro, String usuarioId, String fecha) throws RemoteException;
 }
